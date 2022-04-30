e51d48fe955fabe821aabda312b5deb0a3216d7c
Dorina Slovineanu <dorina.slovineanu@amdaris.com>
2022-04-30 08:31:59 +0300

    HW part 3

---
+++ Exercise2/Exercise2/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise2
+{
+    class Student : User
+    {
+        private int _scholarship;
+        private string _course;
+
+        public Student(string name, int age, int bourse, string course)
+        {
+            _scholarship = bourse;
+            _course = course;
+            base._name = name;
+            base._age = age;
+        }
+
+        public void PrintStudentInfo()
+        {
+            Console.WriteLine("Info about student: " + _name + "\nCourse name - " + _course + "\nScloarship in $ = " + _scholarship);
+        }
+
+        public override void CalculateSum(int x, int y)
+        {
+            Console.WriteLine("The diference is: " + (x - y));
+        }
+    }
+}
---
+++ Exercise2/Exercise2/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise2
+{
+    class User : Education
+    {
+        protected string _name;
+        protected int _age;
+        DateTime thisDay = DateTime.Today;
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void GetName()
+        {
+            Console.WriteLine(_name);
+        }
+
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+        public void GetAge()
+        {
+            Console.WriteLine(_age);
+
+        }
+
+        public override void PrintTodayDate()
+        {
+            Console.WriteLine(thisDay.ToString("D"));
+        }
+
+        public virtual void CalculateSum(int x, int y)
+        {
+            Console.WriteLine("The sum is: " + (x + y));
+        }
+    }
+}
